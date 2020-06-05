886fe90d760c599c8e5f63ceaf7049bca81b18f8
Web-zhiy-Jiang <3474449640@qq.com>
2020-06-05 23:00:55 +0800

    ???

---
+++ meituan/src/views/About.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- <div>{{message}}</div> -->
+    <div v-for='(item) in items' :key="item">
+      {{item}}
+    </div>
+    <input type="checkbox" id='isTrue' value="adq" v-model="isTrue">
+    <label for="isTrue">{{isTrue}}</label>
+    <input type="text" value="ahjk">
+    <p>
+      <input type="checkbox"  id="jspan" value="jspans" v-model="web_name">
+      <label for="isTrue">jspan</label>
+      <input type="checkbox"  id="Panda" value="Panda" v-model="web_name">
+      <label for="isTrue">Panda</label>
+      <input type="checkbox"  id="panpan" value="pampan" v-model="web_name">
+      <label for="isTrue">panpan</label>
+    </p>
+    <p>
+      {{web_name}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'about',
+  data () {
+    return {
+      items:[1,3,4,5,6,7,8,1],
+      isTrue:true,
+      // isTrue:[],
+      web_name: []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
